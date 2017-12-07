5432ea0e35dd214205e0c8c23b3d1ad59e4b8e0c
James <dickfala@gmail.com>
2017-12-07 20:57:20 +0800

    Initial Commit

---
+++ TimerSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TimerSample
+//
+//  Created by cyy on 2017/12/7.
+//  Copyright © 2017年 james chang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
